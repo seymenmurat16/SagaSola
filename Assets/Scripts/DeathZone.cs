e8823326518513b9e7e56684def87e685816be9b
Metin Mert Akçay <metinmertakcayhpalytu@gmail.com>
2019-05-10 11:08:44 +0300

    changes added

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public GameObject guiGameOver = null;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other != null){
+            Destroy(other.gameObject);
+            if (other.gameObject.tag == "Player"){
+                guiGameOver.SetActive ( true );   
+            }
+        }
+    }
+}
